63b63b6578e91979aa0f9018353e8d61494a7512
Sami DURUR <durursami@gmail.com>
2022-09-22 16:20:11 +0300

    Ortalama hesaplama

---
+++ CSharpProjeleri/KolaySeviyeProjeler/KolaySeviye/OrtalamaHesapla/OrtalamaHesapla.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolaySeviye.OrtalamaHesaplama
+{
+    public class OrtalamaHesapla
+    {
+        private uint derinlik = 1;
+        public void Derinlik()
+        {
+            Matematik fibonacciHesaplama = new();
+            bool dogrugiris;
+            Console.Write("Sırasıyla kaç tane fibonacci sayısının ortalaması alınsın :");
+            dogrugiris = uint.TryParse(Console.ReadLine(), out derinlik);
+            if (!dogrugiris)
+            {
+                Console.WriteLine("Yanlış tuşlama yapıldı.Tekrar denemek için bir tuşa basın");
+                Console.ReadKey();
+                Derinlik();
+            }
+            else
+                fibonacciHesaplama.Fibonacci(Convert.ToInt32(derinlik));
+
+        }
+    }
+    public class Matematik
+    {
+        private List<int> sayiList = new List<int>();
+        public void Fibonacci(int derinlik)
+        {
+            int fibonacci1 = 1;
+            int fibonacci2 = 1;
+            int fibonacci3;
+            sayiList.Clear();
+            for (int i = 0; i < derinlik; i++)
+            {
+                sayiList.Add(fibonacci1);
+                fibonacci3 = fibonacci1 + fibonacci2;
+                fibonacci1 = fibonacci2;
+                fibonacci2 = fibonacci3;
+            }
+            Ortalama(derinlik, sayiList);
+        }
+        private void Ortalama(int sayiAdeti, List<int> ortalamaList)
+        {
+            int toplam = 0;
+            foreach (var i in ortalamaList)
+                toplam += i;
+            int ortalama = toplam / sayiAdeti;
+            Console.WriteLine("Sayıların ortalaması :" + ortalama);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpProjeleri/KolaySeviyeProjeler/KolaySeviye/Program.cs
@@ -0,0 +1,7 @@
+﻿using KolaySeviye;
+using KolaySeviye.OrtalamaHesaplama;
+
+OrtalamaHesapla ortalamaHesaplama = new();
+Console.WriteLine("derinlik hesaplamak için dıkla");
+Console.ReadKey();
+ortalamaHesaplama.Derinlik();
